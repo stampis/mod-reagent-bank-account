3549a71ee2430e6125ef464b8c0951630a494503
stampis <jim_axling@hotmail.com>
2026-04-25 16:14:36 +0200

    phaseMask fix

--- data/sql/world/base/reagent_bank_account_NPC.sql
+++ data/sql/world/base/reagent_bank_account_NPC.sql
@@ -43,14 +43,14 @@ INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`,
 -- Reagent Banker - Spawns
 DELETE FROM `creature` WHERE (`id1` = @Entry);
 INSERT INTO `creature` (`id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
-(@Entry, 0, 0,571 , 0, 0, 1, 1,0, 5630.46, 676.087, 651.994, 0.455004, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Dalaran (Eventide Bank)
+(@Entry, 0, 0, 571, 0, 0, 1, 1,0, 5630.46, 676.087, 651.994, 0.455004, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Dalaran (Eventide Bank)
 (@Entry, 0, 0, 571, 0, 0, 1, 1, 0, 5977.53, 626.315, 650.627, 3.61449, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Dalaran (Antonidas Bank)
-(@Entry, 0, 0, 0, 0, 0, 1, 262144, 0, -8918.87, 621.708, 99.523, 0.464504, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Stormwind (Bank)
-(@Entry, 0, 0, 0, 0, 0, 1, 262144, 0, -8806.92, 672.932, 96.5986, 4.26725, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Stormwind (Auctionhouse)
+(@Entry, 0, 0, 0, 0, 0, 1, 1, 0, -8918.87, 621.708, 99.523, 0.464504, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Stormwind (Bank)
+(@Entry, 0, 0, 0, 0, 0, 1, 1, 0, -8806.92, 672.932, 96.5986, 4.26725, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Stormwind (Auctionhouse)
 (@Entry, 0, 0, 0, 0, 0, 1, 1, 0, -4885.07, -989.828, 503.941, 2.87485, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Ironforge (Bank)
 (@Entry, 0, 0, 1, 0, 0, 1, 1, 0, 9940.66, 2504.43, 1318, 3.52402, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Darnassus (Bank)
 (@Entry, 0, 0, 0, 0, 0, 1, 1, 0, -14424, 510.607, 4.94254, 1.50284, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Bootybay (Auctionhouse)
 (@Entry, 0, 0, 1, 0, 0, 1, 1, 0, -7235.01, -3808.31, -1.13622, 0.32184, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Gadgetzan (Auctionhouse)
-(@Entry, 0, 0, 530, 0, 0, 1, 65536, 0, -2010.2, 5364.5, -9.3506, 6.04988, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Shattrath (Scryer Bank)
-(@Entry, 0, 0, 530, 0, 0, 1, 65536, 0, -1723.69, 5506.06, -9.79927, 3.98036, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Shattrath (Aldor Bank)
+(@Entry, 0, 0, 530, 0, 0, 1, 1, 0, -2010.2, 5364.5, -9.3506, 6.04988, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Shattrath (Scryer Bank)
+(@Entry, 0, 0, 530, 0, 0, 1, 1, 0, -1723.69, 5506.06, -9.79927, 3.98036, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Shattrath (Aldor Bank)
 (@Entry, 0, 0, 530, 0, 0, 1, 1, 0, -702.862, 2705.06, 94.6871, 5.09602, 300, 0, 0, 120, 0, 0, 0, 0, 0, '', NULL, 0, NULL); -- Honor Hold (Inn)
